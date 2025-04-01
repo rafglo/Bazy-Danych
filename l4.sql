6483a9837c1c502a2bf3ed7a783ad9e23e693be0
rafglo <127320711+rafglo@users.noreply.github.com>
2025-04-01 11:00:03 +0200

    Update l4.sql

--- l4.sql
+++ l4.sql
@@ -25,6 +25,39 @@ GROUP BY customer_id
 ORDER BY doplata DESC;
 
 --4.4
-SELECT city_id,
-GROUP_CONCAT(district ORDER BY district SEPARATOR ' ORAZ ')
-WHERE 
+SELECT city_id, 
+       GROUP_CONCAT(DISTINCT district ORDER BY district SEPARATOR ' oraz ') AS dystrykty
+FROM address
+GROUP BY city_id
+HAVING COUNT(DISTINCT district) > 1
+ORDER BY city_id DESC;
+
+--4.5
+SELECT amount AS kwota_wypozyczenia, 
+       staff_id AS pracownik, 
+       SUM(amount) AS zarobek
+FROM payment
+GROUP BY amount, staff_id
+ORDER BY zarobek DESC;
+
+--4.6
+SELECT MIN(IF(return_date IS NULL, 0, TIMESTAMPDIFF(SECOND, rental_date, return_date))) AS najkrotsze, 
+MAX(IF(return_date IS NULL,TIMESTAMPDIFF(SECOND, rental_date, '2005-09-02 02:35:22') , TIMESTAMPDIFF(SECOND, rental_date, return_date))) AS najdluzsze
+FROM rental
+WHERE return_date IS NOT NULL;
+
+--4.7
+SELECT customer_id, 
+AVG(IF(return_date IS NULL,TIMESTAMPDIFF(SECOND, rental_date, '2005-09-02 02:35:22') , TIMESTAMPDIFF(SECOND, rental_date, return_date))) / 86400 AS sredni_czas_dni
+FROM rental
+GROUP BY customer_id
+ORDER BY sredni_czas DESC
+LIMIT 1;
+
+--4.8
+SELECT customer_id, 
+SUM(IF(return_date IS NULL,TIMESTAMPDIFF(SECOND, rental_date, '2005-09-02 02:35:22') , TIMESTAMPDIFF(SECOND, rental_date, return_date)) / 2629800) AS w_sumie_miesiecy
+FROM rental
+GROUP BY customer_id
+ORDER BY w_sumie DESC
+LIMIT 1;
