b73b70e499e2e560c697feae921938884b8d56c4
rafglo <127320711+rafglo@users.noreply.github.com>
2025-06-05 16:35:00 +0200

    ed

---
+++ l8.sql
@@ -0,0 +1,88 @@
+-- 8.1
+CREATE TEMPORARY TABLE actor_276020 (
+    actor_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    imię VARCHAR(45) NOT NULL,
+    nazwisko VARCHAR(45) NOT NULL,
+    last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (actor_id)
+);
+
+INSERT INTO actor_276020 (imię, nazwisko)
+SELECT first_name, last_name
+FROM sakila.actor;
+
+SELECT * FROM actor_276020;
+
+-- 8.2
+CREATE TEMPORARY TABLE film_276020 AS
+SELECT * FROM sakila.film;
+
+CREATE TEMPORARY TABLE film_actor_276020 AS
+SELECT * FROM sakila.film_actor;
+
+CREATE TEMPORARY TABLE category_276020 AS
+SELECT * FROM sakila.category;
+
+CREATE TEMPORARY TABLE film_category_276020 AS
+SELECT * FROM sakila.film_category;
+
+CREATE TEMPORARY TABLE customer_276020 AS
+SELECT * FROM sakila.customer;
+
+-- 8.3
+INSERT INTO actor_276020 (imię, nazwisko) VALUES
+('Ryszard', 'Kotys'),
+('Andrzej', 'Grabowski'),
+('Dariusz', 'Gnatowski');
+
+SELECT *
+FROM actor_276020
+ORDER BY actor_id DESC
+LIMIT 3;
+
+--8.4
+CREATE TEMPORARY TABLE fav_film_fans_276020 AS
+SELECT DISTINCT
+    customer.customer_id,
+    customer.first_name,
+    customer.last_name
+FROM
+    customer_276020 AS customer
+JOIN
+    sakila.rental 
+ON 
+    customer.customer_id = rental.customer_id
+JOIN
+    sakila.inventory
+ON 
+    rental.inventory_id = inventory.inventory_id
+JOIN
+    film_276020 AS film
+ON 
+    inventory.film_id = film.film_id
+WHERE
+    film.title = 'ACADEMY DINOSAUR';
+
+SELECT * FROM fav_film_fans_276020;
+
+-- 8.5
+ALTER TABLE fav_film_fans_276020
+ADD COLUMN film_fan_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST;
+
+--8.6
+UPDATE actor_276020
+SET nazwisko = (
+    SELECT nazwisko FROM actor_276020 WHERE actor_id = 25
+)
+WHERE imię = "Sandra" AND nazwisko = "Kilmer";
+
+--8.7
+ALTER TABLE actor_276020
+ADD COLUMN
+    played_in_1 = 1
+WHERE
+    actor_id IN (
+        SELECT fa.actor_id
+        FROM film_actor_276020 fa
+        WHERE fa.film_id = 1
+    );
